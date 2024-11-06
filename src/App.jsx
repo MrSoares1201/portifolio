8d745c6dd9f2a87abba40ef40a6bb78c1832ce1e
MrSoares1201 <eduardomarcos819@gmail.com>
2024-11-06 09:17:03 -0300

    viadao
    Samuel é meu amor

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header_link from './conponents/headder/links'
+import Sobre from './conponents/sobre/sobre'
+import Contato from './conponents/contato/contato'
+import Projetos from './conponents/projetos/projects'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Header_link/>
+        <Routes>
+          <Route path='/' element={<Sobre />} />
+          <Route path='/cnt' element={<Contato />} />
+          <Route path='/prj' element={<Projetos />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
